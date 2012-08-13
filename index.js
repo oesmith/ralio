f93ff50509f2b3fbf98e1e388946deedbaef4ca1
Olly Smith <olly.smith@gmail.com>
2012-08-13 22:21:59 +0100

    Initial.

---
+++ index.js
@@ -0,0 +1,129 @@
+var url = require('url'),
+    querystring = require('querystring'),
+    request = require('request'),
+    _ = require('underscore');
+
+function Ralio(username, password) {
+  this.username = username;
+  this.password = password;
+}
+
+Ralio.prototype.bulkUrl = function () {
+  return url.format({
+    protocol: 'https',
+    auth: this.username + ':' + this.password,
+    hostname: 'rally1.rallydev.com',
+    pathname: '/slm/webservice/1.36/adhoc.js'
+  });
+};
+
+Ralio.prototype.date = function () {
+  return new Date().toISOString().slice(0, 10);
+}
+
+Ralio.prototype.bulk = function (query, callback) {
+  var data = {};
+  for (var key in query) {
+    if (query.hasOwnProperty(key)) {
+      if (typeof query[key] === 'object') {
+        data[key] = '/' + key + '?' + querystring.stringify(query[key]);
+      }
+      else {
+        data[key] = query[key];
+      }
+    }
+  }
+
+  var options = {
+    url: this.bulkUrl(),
+    method: 'POST',
+    json: data,
+    strictSSL: true
+  };
+
+  request(options, function (error, response, data) {
+    callback(error, data);
+  });
+};
+
+Ralio.prototype.projects = function (callback) {
+  this.bulk(
+    {
+      project: {
+        fetch: 'Name',
+        order: 'Name',
+        pagesize: 100
+      }
+    },
+    function (error, data) {
+      if (error !== null) {
+        callback(error);
+      }
+      else {
+        callback(null, data.projects.Results);
+      }
+    }
+  )
+};
+
+Ralio.prototype.backlog = function (projectName, callback) {
+  this.bulk(
+    {
+      hierarchicalrequirement: {
+        fetch: 'Name,FormattedID,Rank,Tasks',
+        order: 'Rank',
+        query: '((Project.Name = "' + projectName + '") AND (Iteration = NULL))',
+        pagesize: 100
+      },
+      defect: {
+        fetch: 'Name,FormattedID,Rank,Tasks',
+        order: 'Rank',
+        query: '((Project.Name = "' + projectName + '") AND (Iteration = NULL))',
+        pagesize: 100
+      }
+    },
+    function (error, data) {
+      if (error !== null) {
+        callback(error);
+      }
+      else {
+        var stories = data.hierarchicalrequirement.Results.concat(data.defect.Results);
+        stories = _.sortBy(stories, function (s) { return s.Rank });
+        callback(null, stories);
+      }
+    }
+  );
+};
+
+Ralio.prototype.sprint = function (projectName, callback) {
+  var d = this.date();
+  this.bulk(
+    {
+      hierarchicalrequirement: {
+        fetch: 'Name,FormattedID,Rank,Tasks',
+        order: 'Rank',
+        query: '((Project.Name = "' + projectName + '") AND ((Iteration.StartDate <= "' + d + '") AND (Iteration.EndDate >= "' + d + '")))',
+        pagesize: 100
+      },
+      defect: {
+        fetch: 'Name,FormattedID,Rank,Tasks',
+        order: 'Rank',
+        query: '((Project.Name = "' + projectName + '") AND ((Iteration.StartDate <= "' + d + '") AND (Iteration.EndDate >= "' + d + '")))',
+        pagesize: 100
+      }
+    },
+    function (error, data) {
+      if (error !== null) {
+        callback(error);
+      }
+      else {
+        var stories = data.hierarchicalrequirement.Results.concat(data.defect.Results);
+        stories = _.sortBy(stories, function (s) { return s.Rank });
+        callback(null, stories);
+      }
+    }
+  );
+}
+
+module.exports = Ralio;
+
